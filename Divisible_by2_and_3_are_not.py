136faf947fef4055d806fef3d111db0ffcb11086
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-12 12:11:21 +0530

    Divisible by 2 and 3

--- Divisible_by2_and_3_are_not.py
+++ Divisible_by2_and_3_are_not.py
@@ -1,8 +1,8 @@
-# num = int(input("Enter a number: "))
-# if num % 2 == 0 and num % 3 == 0:
-#     print(num," is divisible by 2 and 3")
-# else:
-#     print(num,"is not divisible by 2 and 3")
+num = int(input("Enter a number: "))
+if num % 2 == 0 and num % 3 == 0:
+    print(num," is divisible by 2 and 3")
+else:
+    print(num,"is not divisible by 2 and 3")
 
 # 2nd way
 
