048c29ac31e2b1909e539d5a5d28f919bcc78072
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-12 12:08:16 +0530

    Largest number between two numbers

--- Largest_number.py
+++ Largest_number.py
@@ -4,3 +4,11 @@ if a > b:
     print(a,"is largest number")
 else:
     print(b,"is largest number")
+
+def largest_number(a, b):
+    if a > b:
+        return a,"is a largest number"
+    else:
+        return b,"is a largest number"
+
+print(largest_number(12,14))
