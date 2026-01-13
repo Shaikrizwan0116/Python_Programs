5b9dd18bf1399e98c13d41dcde31a76eb53508a8
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-13 17:28:06 +0530

    Buzz number

--- Buzz_number.py
+++ Buzz_number.py
@@ -1,5 +1,14 @@
-num = int(input("Enter a number: "))
-if num % 7 == 0 or num % 10 == 7:
-    print(num," is buzz number")
-else:
-    print(num,"is not a buzz number")
+# num = int(input("Enter a number: "))
+# if num % 7 == 0 or num % 10 == 7:
+#     print(num," is buzz number")
+# else:
+#     print(num,"is not a buzz number")
+
+
+def buzz_number(num):
+    if num % 7 == 0 or num % 10 == 7:
+        return num,"is buzz number"
+    else:
+        return num,"is not buzz number"
+
+print(buzz_number(49))
