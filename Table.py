f0a556948b99776121043ebd797205c5de939bdc
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-14 16:06:31 +0530

    Table

---
+++ Table.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a Table: "))
+s = int(input("Enter a number: "))
+for i in range(1,s+1):
+    print(num,"*",i,"=",num*i)
+
+def table(num,s):
+    for i in range(1,s+1):
+        print(num,"*",i,"=",num*i)
+
+print(table(5,10))
