e5c89a4169e02e79c53ec7ba584a80a9e68a1465
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-18 15:08:32 +0530

    Half_Pyramid by using stars

---
+++ Half_pyramid.py
@@ -0,0 +1,15 @@
+# num = int(input("Enter a number: "))
+
+# for row in range(num):
+#     for col in range(row+1):
+#         print("*", end=" ")
+#     print()
+
+def half_pyramid(num):
+    if num > 0:
+        for row in range(num):
+            for col in range(row+1):
+                print("*", end=" ")
+            print()
+
+half_pyramid(5)
