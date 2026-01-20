eacd2552bdd396fc480498b9e1f355a8dbf4be34
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-20 21:33:22 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,15 @@
+tupl = (1,2,3,4,5,"Rizwan",3+4j)
+print(tupl)
+print(type(tupl))
+
+print(tupl[0:5])
+
+print(tupl[:-1])
+
+print(tupl[0::2])
+
+print(tupl[0:5:2])
+
+print(type(tupl[5]))
+
+print(len(tupl))
