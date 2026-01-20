bc7ec5c785401156f3ec6a66ebae97aba2373094
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-20 22:09:37 +0530

    Adding list items

--- Add_list_items.py
+++ Add_list_items.py
@@ -1,8 +1,22 @@
-def list_items(list1):
-    sum = 0
+# def list_items(list1):
+#     sum = 0
+#     for i in list1:
+#         sum += i
+#     return sum
+# list1 = [1,2,3,4]
+# a = list_items(list1)
+# print("The list items", a)
+
+def even_odd(list1):
+    even = 0
+    odd = 0
     for i in list1:
-        sum += i
-    return sum
-list1 = [1,2,3,4]
-a = list_items(list1)
-print("The list items", a)
+        if i % 2 == 0:
+            even += i
+        else:
+            odd += i
+    return even, odd
+list1 = [1,2,3,4,5,6,7,8,9,10]
+e,o = even_odd(list1)
+print("The Even numbers count",e)
+print("The odd number count",o)
