63a8910c065e8d8b20c282510ba3bdf94a6c62e0
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-21 21:23:24 +0530

    Lambda function

---
+++ Lambda.py
@@ -0,0 +1,18 @@
+#Lambda function: 
+    # 1.lambda function doesn't define def keyword
+    # 2.lambda keyboard
+    # 3.it returns an expression
+    # 4.it doesn't allow global variable
+    # 5.single expression
+    # 6.any number of arguments
+    # 7.it takes only one expression
+    # 8.lambda arguments:expression
+
+
+# y = lambda a,b:a+b
+# sum = y(1,2)
+# print(sum)
+
+y = lambda a,b:a*b
+mul = y(2,2)
+print(mul)
