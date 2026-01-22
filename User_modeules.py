d573d853c2921ee6fde078ad66e822732d64d988
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-22 16:22:30 +0530

    User defined module

---
+++ User_modeules.py
@@ -0,0 +1,11 @@
+#User defined modules
+
+def add(a,b):
+    return a+b
+def sum(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a//b
+    
---
+++ User_modules1.py
@@ -0,0 +1,6 @@
+from User_modeules import add
+
+a= int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+sum = add(a,b)
+print("The sum of number",sum)
