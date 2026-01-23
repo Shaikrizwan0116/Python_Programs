0bfd6c28c503367a36904e110f4d9022d53684ae
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2026-01-23 18:53:38 +0530

    Decorator

--- Decorator.py
+++ Decorator.py
@@ -2,17 +2,17 @@
 
 #Beginner Style:
 
-# def display():
-#     print("This is decorator")
+def display():
+    print("This is decorator")
 
-# def display1(fun):
-#     def smartdisplay():
-#         print("*****")
-#         fun()
-#         print("*****")
-#     return smartdisplay
-# a = display1(display)
-# a()
+def display1(fun):
+    def smartdisplay():
+        print("*****")
+        fun()
+        print("*****")
+    return smartdisplay
+a = display1(display)
+a()
 
 
 def display1(fun):
@@ -26,3 +26,20 @@ def display1(fun):
 def display():
     print("The is showing something")
 display()
+
+## Decorators with parameters
+
+def smart_div(fun):
+    def inner_func(a,b):
+        print("Welcome to PT")
+        if b == 0:
+            print("Cant divided")
+            return
+        return fun(a,b)
+    return inner_func
+
+@smart_div
+def div(a,b):
+    return a//b
+result_div = div(15,3)
+print(result_div)
